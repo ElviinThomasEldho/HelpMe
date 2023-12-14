46694905c23b21f0955d30da5e2bab2dafd6fe44
ElviinThomasEldho <elviin.t.eldho@gmail.com>
2023-12-14 16:58:23 +0530

    vercel configurations

--- helpme/helpme/wsgi.py
+++ helpme/helpme/wsgi.py
@@ -1,7 +1,5 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helpme.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helpme.settings")
 application = get_wsgi_application()
+app = application
